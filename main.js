090faf5c2bf6f3758b60230c9c351918c0f1ffec
Sohan <roarceus@users.noreply.github.com>
2020-12-08 17:44:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9hcmNldXMiLCJhIjoiY2tpZnZsZ21xMGk4YjJzcWtzemI5bWJnaCJ9.5jNNbddjJTfvQMU3yUW77Q';
+
+navigator.geolocation.getCurrentPosition(locationSuccess, locationError, { enableHighAccuracy: true});
+
+function locationSuccess(position) {
+    displayMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function locationError() {
+    displayMap([-0.12838, 51.51574]);
+}
+
+function displayMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
+    });
+    
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-right');
+
+    var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    });
+
+    map.addControl(directions, 'top-left');
+}
+
